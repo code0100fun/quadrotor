8416b363eae354b3f40e54c737fe112093c389eb
Chase McCarthy <chase@code0100fun.com>
2016-02-15 23:35:23 -0500

    Initial commit

---
+++ ember-cli-build.js
@@ -0,0 +1,81 @@
+/*jshint node:true*/
+/* global require, module */
+var existsSync = require('exists-sync');
+var merge = require('broccoli-merge-trees');
+var transpileES6 = require('emberjs-build/lib/utils/transpile-es6');
+var stew = require('broccoli-stew');
+var concat = require('broccoli-concat');
+var config = require('./package.json');
+var find = stew.find;
+
+function transpile(tree, label) {
+  return transpileES6(tree, label, { avoidDefine: false, sourceMaps: 'inline' });
+}
+
+module.exports = function() {
+  var packages = __dirname + '/node_modules';
+  var bower = __dirname + '/bower_components';
+  var hasBower = existsSync(bower);
+
+  var libTree = find('lib', {
+    include: ['**/*.js']
+  });
+
+  libTree = transpile(libTree, config.name);
+
+  libTree = concat(libTree, {
+    inputFiles: ['**/*.js'],
+    outputFile: '/amd/' + config.name + '.amd.js',
+    sourceMapConfig: {
+      enabled: true,
+      cache: null,
+      sourceRoot: '/'
+    }
+  });
+
+  var testHarnessTrees = [
+      find(__dirname + '/tests', {
+      srcDir: '/',
+      files: [ 'index.html' ],
+      destDir: '/tests'
+    })
+  ];
+
+  if (hasBower) {
+    testHarnessTrees.push(find(bower, {
+      srcDir: '/qunit/qunit',
+      destDir: '/tests'
+    }));
+  }
+
+  var testHarness = merge(testHarnessTrees);
+  var tests = find('tests', { include: ['**/*-test.js', 'helpers/**/*.js'] });
+  tests = transpile(tests, 'tests');
+  tests = concat(tests, {
+    inputFiles: ['**/*.js'],
+    outputFile: '/amd/' + config.name + '-tests.amd.js',
+    sourceMapConfig: {
+      enabled: true,
+      cache: null,
+      sourceRoot: '/'
+    }
+  });
+
+  var finalTrees = [
+    libTree,
+    testHarness,
+    tests
+  ];
+
+  if (hasBower) {
+    var loader = find(bower, {
+      srcDir: '/loader.js',
+      files: [ 'loader.js' ],
+      destDir: '/assets'
+    });
+
+    finalTrees.push(loader);
+  }
+
+  return merge(finalTrees);
+};
---
+++ tests/unit/quadrotor-test.js
@@ -0,0 +1,7 @@
+import quadrotor from 'quadrotor';
+
+QUnit.module('Quadrotor');
+
+test("it works", function(assert) {
+  assert.equal(1, 1);
+});
