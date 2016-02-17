1b37d218d0ef1568e311fed57a236cc51b41b534
Chase McCarthy <chase@code0100fun.com>
2016-02-16 22:46:26 -0500

    Add ZXY rotation matrix to Euler util

--- tests/unit/utils/rotation-test.js
+++ tests/unit/utils/rotation-test.js
@@ -1,4 +1,4 @@
-import { eulerToZXYRotation } from 'utils/rotation';
+import { eulerToZXYRotation, zxyRotationToEuler } from 'utils/rotation';
 
 QUnit.module('Util - rotation');
 
@@ -15,4 +15,25 @@ test("eulerToZXYRotation(pi/4,-pi/8,pi)", function(assert) {
                               [ 0.382683432365090,  0.653281482438188,  0.653281482438188
                               ]]);
   assert.matrixEqual(R, expected);
+});
+
+test("zxyRotationToEuler(eye(3))", function(assert) {
+  let [phi, theta, psi] = zxyRotationToEuler(math.eye(3));
+  let expected = [0, 0, 0];
+  assert.equal(phi, 0);
+  assert.equal(theta, 0);
+  assert.equal(psi, 0);
+});
+
+test("zxyRotationToEuler(/* arbitarty R */)", function(assert) {
+  // [phi, theta, psi] = 30,45,90
+  let R = math.matrix([
+                        [ 0.516217586476753,  0.846340372739808, -0.131253102373843],
+                        [-0.137900281555049, -0.069116004944297, -0.988031624092862],
+                        [-0.845282743086096,  0.528139140124250,  0.081031678432964
+                      ]]);
+  let [phi, theta, psi] = zxyRotationToEuler(R);
+  assert.close(phi, -1.415926535897933, 1e-14);
+  assert.close(theta, 1.017702849742895, 1e-14);
+  assert.close(psi, 2.035405699485789, 1e-14);
 });
